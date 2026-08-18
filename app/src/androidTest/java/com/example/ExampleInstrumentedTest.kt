0060d6d257832d581d8f7b2b9820b95f703c8ad2
aarpar11 <aarushparikh11@gmail.com>
2026-08-18 15:55:14 -0500

    Clean up comments in ExampleInstrumentedTest
    Removed unnecessary comments from the ExampleInstrumentedTest class.

--- app/src/androidTest/java/com/example/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/ExampleInstrumentedTest.kt
@@ -7,10 +7,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 /**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+ * Test
+ **/
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
   @Test
